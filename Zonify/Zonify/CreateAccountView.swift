b6d24a554c3c01be9befa44fb78e633df3c46068
CB915 <101878146+CB915@users.noreply.github.com>
2023-07-21 20:59:57 -0400

    Changes to UI colors

--- Zonify/Zonify/CreateAccountView.swift
+++ Zonify/Zonify/CreateAccountView.swift
@@ -13,92 +13,100 @@ struct CreateAccountView: View {
     @State private var password = ""
 
     var body: some View {
-
-        VStack{
-            Spacer()
-            //-------------------------
-            // Page Title
-            //-------------------------
-            VStack{
-                Spacer()
-                Text("New Account").font(.largeTitle)
-                    .bold().foregroundColor(.pink)
-                    .opacity(0.8)
-                Spacer()
-
-            }// VStack page title
-            Spacer()
-            //-------------------------
-            // Email
-            //-------------------------
-            VStack{
-                HStack{
-                    Image(systemName: "mail").foregroundColor(.gray)
-                    TextField("Email", text: $email)
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .fontWeight(.bold)
-                }.padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 4)
-                            .foregroundColor(.pink).opacity(0.8)).padding()
-                //-------------------------
-                // Password
-                //-------------------------
-                HStack{
-                    Image(systemName: "lock")
-                        .foregroundColor(.gray)
-                    TextField("Password", text: $password)
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .fontWeight(.bold)
-                }.padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 4)
-                            .foregroundColor(.pink).opacity(0.8)
-                    ).padding()
-            }// Vstack email , password
+        ZStack{
+           // Image("bg-gradient-2").resizable().edgesIgnoringSafeArea(.all)
             
-            //-------------------------
-            // Create account button
-            //-------------------------
             VStack{
-                Button {
-                    print("Create account: \(email) \t \(password)")
-                } label: {
-                    Text("Create Account")
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .bold()
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 25)
-                            .fill(.pink)
-                            .foregroundColor(.pink)
-                            .opacity(0.8)
-                        ).padding(.horizontal)
-                }
-                .padding()
-                .padding(.top)
-                
+                Spacer()
                 //-------------------------
-                // Return Button
+                // Page Title
                 //-------------------------
-                
                 VStack{
-                    Button( action: {
-                        presentationMode.wrappedValue.dismiss()
-                    }) {
-                        
-                        Text("Return")
-                            .foregroundColor(.gray)
-                            .font(.title)
-                        
-                    }
                     Spacer()
-                }//end of button vstack
-            }
-        }.background(Color.white).edgesIgnoringSafeArea(.all)//end VStack main
+                    Text("Create a New Account").font(.largeTitle)
+                        .bold().foregroundColor(.white)
+                        .opacity(0.8)
+                    Spacer()
+
+                }// VStack page title
+                Spacer()
+                //-------------------------
+                // Email
+                //-------------------------
+                VStack{
+                    HStack{
+                        Image(systemName: "mail").foregroundColor(.gray)
+                        TextField("Email", text: $email)
+                            .foregroundColor(Color.white)
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }.padding()
+                        .background((Color.white).cornerRadius(10).opacity(0.75))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2)
+                                .foregroundColor(.white).opacity(0.75)).padding()
+                    //-------------------------
+                    // Password
+                    //-------------------------
+                    HStack{
+                        Image(systemName: "lock")
+                            .foregroundColor(.gray)
+                        TextField("Password", text: $password)
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }.padding()
+                        .background((Color.white).cornerRadius(10).opacity(0.75))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2)
+                                .foregroundColor(.white).opacity(0.75)
+                        ).padding()
+                }// Vstack email , password
+                
+                //-------------------------
+                // Create account button
+                //-------------------------
+                VStack{
+                    Button {
+                        print("Create account: \(email) \t \(password)")
+                    } label: {
+                        Text("Create Account")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .bold()
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 25)
+                                .fill(Color.init(hue: 0.9, saturation: 0.9, brightness: 0.8))
+                                .foregroundColor(.white)
+                                .opacity(1)
+                            ).padding(.horizontal)
+                    }
+                    .padding()
+                    .padding(.top)
+                    
+                    //-------------------------
+                    // Return Button
+                    //-------------------------
+                    
+                    VStack{
+                        Button( action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                            
+                            Text("Return")
+                                .foregroundColor(.white)
+                                .font(.title)
+                            
+                        }
+                        Spacer()
+                    }//end of button vstack
+                }
+            }.background(LinearGradient(gradient: Gradient(colors:[
+                Color.init(hue: 0.72, saturation: 0.8, brightness: 0.3),
+                Color.init(hue: 0.72, saturation: 0.9, brightness: 0.1)]), startPoint: .top, endPoint: .bottom))
+            
+        }
         
     }//end body
 }
